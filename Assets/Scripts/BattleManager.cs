5378db1516dd47825c58d8370685ebbedbe2f1e3
UTKari <denisse.martinez@uteca.edu.mx>
2026-03-02 20:34:58 -0600

    WinScreenSounds
    Agregamos pantalla de win, sonido de win y arreglamos los códigos de battle manager y figther. También añadimos canción de pelea y sonido de win

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -12,7 +12,11 @@ public class BattleManager : MonoBehaviour
     [SerializeField]
     private UnityEvent onStartBattleCount;
     [SerializeField]
+    private UnityEvent onStartBattle;
+    [SerializeField]
     private UnityEvent onStopBattle;
+    [SerializeField]
+    private UnityEvent<string>onWinBattle;
     private Coroutine battleCoroutine;
     private List<Fighter> fighters = new List<Fighter>();
     public void AddFighter(Fighter fighter)
@@ -40,6 +44,7 @@ public class BattleManager : MonoBehaviour
     }
     public void StartBattle()
     {
+        onStartBattle?.Invoke();
         battleCoroutine = StartCoroutine(BattleCoroutine());
     }
     private IEnumerator BattleCoroutine()
@@ -64,8 +69,22 @@ public class BattleManager : MonoBehaviour
             if (defender.Health.CurrentHealth <= 0)
             {
                 RemoveFighter(defender);
+            } 
+            else
+            {
+                yield return new WaitForSeconds(1.5f);
             }
         }
+        if (fighters.Count == 1)
+        {
+            WinBattle(fighters[0]);
+        }
+    }
+    public void WinBattle(Fighter winner)
+    {
+        onWinBattle?.Invoke(winner.CharacterData.characterName);
+        winner.Animator.Play(winner.CharacterData.winAnimationName);
+        winner.transform.LookAt(Camera.main.transform);
     }
     public void StopBattle()
     {
