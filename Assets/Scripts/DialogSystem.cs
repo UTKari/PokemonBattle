13ae31c5b0e3e9b44b90d020e330c50c4d6eb03f
UTKari <denisse.martinez@uteca.edu.mx>
2026-03-09 20:46:36 -0600

    ExamenParte1
    Agregar 2 pkmn mas, animaciones y el código para sonido

--- Assets/Scripts/DialogSystem.cs
+++ Assets/Scripts/DialogSystem.cs
@@ -37,6 +37,7 @@ public class DialogSystem : MonoBehaviour
         foreach (char letter in dialog.ToCharArray())
         {
             dialogText.text += letter;
+            SoundManager.instance.Play("DialogLetter", 1f, Random.Range(0.9f, 1.1f));
             yield return new WaitForSeconds(timerBetweenWords);
         }
         yield return new WaitForSeconds(1f);
