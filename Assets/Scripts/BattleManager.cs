d452933888a36feedc7af5355cb7137740b7f520
UTKari <denisse.martinez@uteca.edu.mx>
2026-03-23 20:10:10 -0600

    a
    a

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -28,6 +28,7 @@ public class BattleManager : MonoBehaviour
             fighter.Initialize();
             if (fighters.Count >= minimumFighters)
             {
+                StopBattle();
                 onStartBattleCount?.Invoke();
             }
         }
@@ -62,6 +63,7 @@ public class BattleManager : MonoBehaviour
             DialogSystem.Instance.ShowDialog(attacker.CharacterData.characterName + " attacks with " + attack.attackData.attackName + "!");
             attacker.transform.LookAt(defender.transform);
             defender.transform.LookAt(attacker.transform);
+            SoundManager.instance.Play(attack.attackData.attackSoundName);
             attacker.Animator.Play(attack.attackData.animationName);
             attack.particlesPool.InstantiateObject(attacker.transform.position);
             float damage = Random.Range(attack.attackData.minDamage, attack.attackData.maxDamage);
